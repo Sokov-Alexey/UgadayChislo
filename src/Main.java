a0a36b5ea5ee759a114f4287f9e27262dab0596c
PC <alsokov2003@gmail.com>
2025-03-01 08:50:37 +0300

    First version

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Угадай число! Укажите через пробел нижний и верхний пределы загадываемого числа");
+        int bottom = scanner.nextInt();
+        int top = scanner.nextInt();
+        Random rand = new Random();
+        int randInt;
+        randInt = rand.nextInt(top) + bottom;
+        System.out.println("Введите количество попыток");
+        int attempt = scanner.nextInt();
+        System.out.println("Введите число");
+        while (attempt >= 1) {
+            int num = scanner.nextInt();
+            attempt -= 1;
+            if (randInt > num) {
+                System.out.println("Загаданное мною число больше");
+            } else if (randInt < num) {
+                System.out.println("Загаданное мною число меньше");
+            } else {
+                System.out.println("Совершенно верно! Это и есть загаданное мною число!");
+                return;
+            }
+        }
+        System.out.println("Попытки закончились, попробуйте ещё раз!");
+    }
+}
